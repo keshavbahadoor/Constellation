97f7cbb0bc4c30622a60b8863330bd30252734f3
keshavbahadoor <keshav.bahadoor@gmail.com>
2015-03-22 16:21:33 -0800

    Initial commit

---
+++ html/src/com/constellation/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.constellation.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.constellation.game.MainGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MainGame();
+        }
+}
